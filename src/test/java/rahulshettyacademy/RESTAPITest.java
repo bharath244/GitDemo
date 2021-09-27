d920e8d93467817812e24834bc6520dbe67badcf
bharath <bhrathm812@gmail.com>
2021-09-27 18:03:45 +0530

    first commit

---
+++ src/test/java/rahulshettyacademy/RESTAPITest.java
@@ -0,0 +1,20 @@
+package rahulshettyacademy;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+	}
+	
+    @Test
+	public void deletTwitter()
+	{
+		System.out.println("deletTwitter");
+	}
+
+
+}
---
+++ src/test/java/rahulshettyacademy/SeleniumTest.java
@@ -0,0 +1,20 @@
+package rahulshettyacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void ExecuteUI()
+	{
+		System.out.println("ExecuteUI");
+	}
+
+
+}
